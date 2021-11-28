269093927569338e18e83902f6f89a8b8cbcca84
Logan Nochez <logannochez@hotmail.fr>
2021-11-28 02:07:14 +0100

    contenu des ingredients "undefined" résolu

--- js/renderCards.js
+++ js/renderCards.js
@@ -16,76 +16,69 @@ class cards {
 
             let card = document.createElement('div')
 
-            // card.className = recipe.ingredients.map(ingredient => ingredient.ingredient.replace(/ /g,"_")).join(" ") +  ' col-3 card'
-
             card.className = 'col-3 card'
 
             card.setAttribute('data-attribute', recipe.id)
+
+            let template = ``
 
             let HTML = `<div class="card-top">
 
                 </div>
                 <div class="card-body">
-                <div class="card-body-top">
-                    <h5>${recipe.name}</h5>
-                    <div class="card-body-top-time">
-                    <img src="images/icons/clock.svg" alt="">
-                    <p>${recipe.time} min</p>
-                    </div>
-                </div>
-
-                <div class="card-body-bottom">
-                    <div class="card-body-bottom-ingredients">
-                    ${recipe.ingredients.map(ingredient =>
-                        
-                        `<p class="card-ingredients__item"><b>${ingredient.ingredient}:</b> ${ingredient.quantity} ${ingredient.unit}</p>`).join(" ")}
+                    <div class="card-body-top">
+                        <h5>${recipe.name}</h5>
+                        <div class="card-body-top-time">
+                            <img src="images/icons/clock.svg" alt="">
+                            <p>${recipe.time} min</p>
+                        </div>
                     </div>
 
-                    <div class="card-body-bottom-description">
-                    <p>${recipe.description}</p>
+                    <div class="card-body-bottom">
+                        <div class="card-body-bottom-ingredients">
+                            ${recipe.ingredients.map(ingredient =>
+                                `<p class="card-ingredients__item">${template = this.ingredientChoiceIfUndefined(ingredient)}</p>`).join(" ")}
+                        </div>
+
+                        <div class="card-body-bottom-description">
+                            <p>${recipe.description}</p>
+                        </div>
                     </div>
-                </div>
-                
-                
-                
+
                 </div>`
 
             cards.appendChild(card)
 
             card.innerHTML = HTML
 
-            
         })
+
     }
 
-    // ingredients(ingredient) {
+    ingredientChoiceIfUndefined(ingredient) {
 
-    //     if (ingredient.quantity.length === 0) {
-    //         let template = 
+        if (ingredient.quantity === undefined) {
+            let template = 
                         
-    //             `<b>${ingredient.ingredient}</b>`
+                `<b>${ingredient.ingredient}</b>`
 
-    //         let ingredientsDiv = document.querySelector('.card-body-bottom-ingredients p')
-    //         console.log(ingredientsDiv)
-    //         ingredientsDiv.innerHTML = template
-
-    //     } if (ingredient.unit.length === 0) {
-    //         let template = 
+            return template
+        } else if (ingredient.unit === undefined) {
+            let template = 
                         
-    //             `<b>${ingredient.ingredient}:</b>${ingredient.quantity}`
+                `<b>${ingredient.ingredient}: </b>${ingredient.quantity}`
 
-    //         let ingredientsDiv = document.querySelector('.card-body-bottom-ingredients p')
-    //         ingredientsDiv.innerHTML = template
-    //     } else {
-    //         let template = 
+            return template
+        } else {
+            let template = 
                         
-    //             `<b>${ingredient.ingredient}:</b>
-    //             ${ingredient.quantity} ${ingredient.unit}`
+                `<b>${ingredient.ingredient}: </b>
+                ${ingredient.quantity} ${ingredient.unit}`
 
-    //         let ingredientsDiv = document.querySelector('.card-body-bottom-ingredients p')
-    //         ingredientsDiv.innerHTML = template
-    //     }
-    // }
+            return template
+        }
+        
+    }
 
 }
 
